0c01e449d418e30f72c7d35ada5703363e689345
Tom Main <tommain1498@gmail.com>
2025-10-05 18:53:49 +0100

    button added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import styled from 'styled-components'
 import { Typo } from './constants/Typography.js'
 import { Colors } from './constants/Colors.js'
 
+import Button from './components/ui/Button.jsx'
+
 function App() {
 
   const StyledHeading = styled.h1`
@@ -14,6 +16,7 @@ function App() {
     <>
       <div>
         <StyledHeading style={Typo.heading.h1}>butthole</StyledHeading>
+        <Button type="primary">Primary Button</Button>
       </div>
     </>
   )
